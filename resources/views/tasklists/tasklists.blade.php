28aad688e7cc77443584aef2359cecb77aab4dcb
ku <piece@ox.interq.or.jp>
2017-07-31 14:11:36 +0000

    kadaikansei2

--- resources/views/tasklists/tasklists.blade.php
+++ resources/views/tasklists/tasklists.blade.php
@@ -7,10 +7,13 @@
                 {!! link_to_route('tasklists.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $tasklist->created_at }}</span>
             </div>
             <div>
+                
                 <p>{!! nl2br(e($tasklist->content)) !!}</p>
+                
             </div>
         </div>
     </li>
 @endforeach
 </ul>
+
 {!! $tasklists->render() !!}
