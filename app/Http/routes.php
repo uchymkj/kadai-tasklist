2a1f11187944feb58ce98d1b204d83e2d0dfda23
ku <piece@ox.interq.or.jp>
2017-08-05 06:28:24 +0000

    kadaitask

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,6 +25,7 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
+
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('tasklists', 'TasklistsController');
 });
